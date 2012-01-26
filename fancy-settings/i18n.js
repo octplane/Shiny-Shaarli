3c755f979a219f3f33a2c9564ffc659e84ffa80e
Pierre Baillet <pierre@baillet.name>
2012-01-26 22:48:40 +0100

    Initial Import

---
+++ fancy-settings/i18n.js
@@ -0,0 +1,72 @@
+// SAMPLE
+this.i18n = {
+    "settings": {
+        "en": "Settings",
+        "de": "Optionen",
+        "fr": "Paramètres"
+    },
+    "Shaarli configuration": {
+        "en": "Shaarli configuration",
+        "fr": "Configuration de Shaarli"
+    },
+    "Server information": {
+        "en": "Server Information",
+        "fr": "Information sur le serveur"
+    },
+    "search": {
+        "en": "Search",
+        "de": "Suche"
+    },
+    "nothing-found": {
+        "en": "No matches were found.",
+        "de": "Keine Übereinstimmungen gefunden."
+    },
+    
+    
+    
+    "information": {
+        "en": "Information",
+        "de": "Information"
+    },
+    "login": {
+        "en": "Login",
+        "de": "Anmeldung"
+    },
+    "username": {
+        "en": "Username:",
+        "de": "Benutzername:"
+    },
+    "password": {
+        "en": "Password:",
+        "de": "Passwort:"
+    },
+    "x-url": {
+        "en": "http://www.domain.com/shaarli/"
+    },
+    "x-characters-pw": {
+        "en": "10 - 18 characters",
+        "de": "10 - 18 Zeichen"
+    },
+    "Base URL": {
+        "en": "Base URL",
+        "fr": "URL racine"
+    },
+    "description": {
+        "en": "Type here the base URL of your Shaarli.<br>\
+            It should look something like <pre>http://www.domain.com/shaarli/</pre>.",
+        "fr": "Saisissez ici l'URL racine de votre Shaarli.<br>\
+            Elle devrait ressembler à :<pre>http://www.domain.com/shaarli/</pre>"
+    },
+    "logout": {
+        "en": "Logout",
+        "de": "Abmeldung"
+    },
+    "enable": {
+        "en": "Enable",
+        "de": "Aktivieren"
+    },
+    "disconnect": {
+        "en": "Disconnect:",
+        "de": "Trennen:"
+    }
+};
---
+++ fancy-settings/manifest.js
@@ -0,0 +1,27 @@
+// SAMPLE
+this.manifest = {
+    "name": "Shiny Shaarly",
+    "icon": "../assets/48x48.png",
+    "settings": [
+        {
+            "tab": i18n.get("Shaarli configuration"),
+            "group": i18n.get("Server information"),
+            "name": "base_url",
+            "type": "text",
+            "label": i18n.get("Base URL"),
+            "text": i18n.get("x-url")
+        },
+        {
+            "tab": i18n.get("Shaarli configuration"),
+            "group": i18n.get("Server information"),
+            "name": "myDescription",
+            "type": "description",
+            "text": i18n.get("description")
+        },
+    ],
+    "alignment": [
+        [
+            "base_url"
+        ]
+    ]
+};
