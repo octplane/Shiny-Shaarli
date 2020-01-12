a6bf0c0b976a9fd6a34a021bdde8bf8b0a5b32e5
Anson VanDoren <anson.vandoren@gmail.com>
2020-01-11 16:02:40 -1000

    fixed formatting to match original

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,30 +1,22 @@
-function populate_iframe() {
+function populate_iframe()
+{
   chrome.tabs.getSelected(null, function(tab) {
-    var settings = new Store('settings', {});
+    var settings = new Store("settings", {});
     var options = settings.toObject();
     var base_url, skip_description;
     console.log(options);
     base_url = options['base_url'];
     skip_description = options['skip_description'];
-    if (base_url == null) {
-      document
-        .getElementById('configure_me')
-        .setAttribute('style', 'display:block');
-      document.getElementById('source').setAttribute('style', 'display:none');
+    if(base_url == null)
+    {
+      document.getElementById('configure_me').setAttribute('style','display:block');
+      document.getElementById('source').setAttribute('style','display:none');
     } else {
-      document
-        .getElementById('configure_me')
-        .setAttribute('style', 'display:none');
-      document.getElementById('source').setAttribute('style', 'display:block');
+      document.getElementById('configure_me').setAttribute('style','display:none');
+      document.getElementById('source').setAttribute('style','display:block');
       var url = tab.url;
       var title = tab.title || url;
-      var if_url =
-        base_url +
-        '/?post=' +
-        encodeURIComponent(url) +
-        '&title=' +
-        encodeURIComponent(title) +
-        '&source=shiny_shaarli';
+      var if_url = base_url + '/?post=' + encodeURIComponent(url)+'&title=' + encodeURIComponent(title)+'&source=shiny_shaarli';
       if (skip_description) {
         if_url += '&description=%20';
       }
@@ -35,4 +27,4 @@ function populate_iframe() {
 
 window.onload = function() {
   populate_iframe();
-};
+}
